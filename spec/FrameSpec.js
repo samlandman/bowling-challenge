653fbea59579936fd353b271d4fd90678f004157
sam landman <sam landman>
2020-07-10 17:07:06 +0100

    Created Frame Class, and added Roll Two

--- spec/FrameSpec.js
+++ spec/FrameSpec.js
@@ -7,9 +7,40 @@ describe("Frame Class", function() {
     frame = new Frame();
   });
 
-  it("should have a default of 'Still to Play'", function() {
+  it("First Roll should have a default of 'Still to Play'", function() {
     expect(frame.firstRoll).toEqual("Still to Play");
   });
+
+  it("Second Roll should have a default of 'Still to Play'", function() {
+    expect(frame.secondRoll).toEqual("Still to Play");
+  });
   
+  it("Roll 1 is 5, and goes into first roll", function() {
+    frame.rollOne(5);
+    expect(frame.firstRoll).toEqual(5);
+  });
+
+  it("Roll 1 is 10, and goes into first roll", function() {
+    frame.rollOne(10);
+    expect(frame.firstRoll).toEqual(10);
+    expect(frame.special).toEqual("Strike");
+  });
+
+  it("Roll 1 is 10, so second roll is 'Strike'", function() {
+    frame.rollOne(10);
+    expect(frame.secondRoll).toEqual("Strike");
+  });
+
+  it("Roll 1 is 10, so second roll should not be allowed", function() {
+    frame.rollOne(10);
+    expect(function() {frame.rollTwo(1)}).toThrow(Error('Strike, so invalid'));
+  });
+
+  it("Roll 1 is 5, and goes into first roll. Roll 2 is a 3 and goes into second roll", function() {
+    frame.rollOne(5);
+    frame.rollTwo(3)
+    expect(frame.firstRoll).toEqual(5);
+    expect(frame.secondRoll).toEqual(3);
+  });
 
 });
--- src/Frame.js
+++ src/Frame.js
@@ -3,5 +3,21 @@
 class Frame {
   constructor() {
     this.firstRoll = 'Still to Play'
+    this.secondRoll = 'Still to Play'
+    this.special = 'normal'
+  };
+  rollOne(number) {
+    this.firstRoll = number
+    if(number==10) {
+      this.special = 'Strike'
+      this.secondRoll = 'Strike'
+    }
+  };
+  rollTwo(number) {
+    if(this.secondRoll=='Strike') {
+      throw Error('Strike, so invalid') ;
+    } else {
+    this.secondRoll = number;
+    };
   };
 };
